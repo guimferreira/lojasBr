9e31de4d51acfbef194bf169a043c94311361ade
Guilherme Medeiros <g_medeiros@outlook.com.br>
2025-11-19 15:11:01 -0300

    first commit

---
+++ src/main/java/com/lojasbrasileiras/lojasbr/LojasbrApplication.java
@@ -0,0 +1,13 @@
+package com.lojasbrasileiras.lojasbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojasbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojasbrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lojasbrasileiras/lojasbr/LojasbrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojasbrasileiras.lojasbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojasbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
